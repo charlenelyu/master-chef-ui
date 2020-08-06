7ca866bca5b96c3bc4f4913253e89f0d0d9d71b5
Tianhui Li <li.tianhu@husky.neu.edu>
2020-08-06 00:21:55 -0700

    edit & delete button UI

--- src/RecipeTable.jsx
+++ src/RecipeTable.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
-import { Card, Col, Row } from 'antd';
+import { Popconfirm, Card, Col, Row } from 'antd';
 import './styles/antStyle.less';
 import { NavLink } from 'react-router-dom';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 
-function RecipeCard({recipe}) {
-  const {id, title, author, img} = recipe;
+function RecipeCard({ recipe }) {
+  const { id, title, author, img } = recipe;
   return (
     <Col className="card-list" span={6}>
       <NavLink to={{ pathname: `/view/${id}`, state: recipe }}>
@@ -16,9 +17,26 @@ function RecipeCard({recipe}) {
           style={{ width: 250, marginLeft: 16 }}
           size="small"
           cover={<img alt={title} src={img} />}
+          actions={[
+            <NavLink to={{ pathname: `/edit/${id}`, state: recipe }}>
+              <EditOutlined key="edit" />
+            </NavLink>,
+            <Popconfirm
+              title="Are you sure delete this task?"
+              // onConfirm={confirm}
+              // onCancel={cancel}
+              okText="Yes"
+              cancelText="No"
+            >
+              <DeleteOutlined key="delete"/>
+            </Popconfirm>,
+          ]}
         >
           {/* author传过来是一个object, 只用里面的name */}
-          <Meta title={title} description={<span>by: {author['name']}</span>}/>
+          <Meta
+            title={title}
+            description={(<span>by: {author['name']}</span>)}
+          />
         </Card>
       </NavLink>
     </Col>
