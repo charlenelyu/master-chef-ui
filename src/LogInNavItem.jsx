dc73f5a0ad5e8241ec7b69afe75bd0b3ea80346a
charlenelyu <lyu.yangh@northeastern.edu>
2020-08-11 13:30:52 -0700

    added register form

--- src/LogInNavItem.jsx
+++ src/LogInNavItem.jsx
@@ -3,24 +3,26 @@ import { NavLink } from 'react-router-dom';
 import { Tabs, Modal, Form, Button, Input, Avatar, Menu, Dropdown } from 'antd';
 import { UserOutlined, DownOutlined } from '@ant-design/icons';
 
-function LogInForm(props) {
-  const layout = {
-    labelCol: {
-      span: 8,
-    },
-    wrapperCol: {
-      span: 16,
-    },
-  };
-  const tailLayout = {
-    wrapperCol: {
-      offset: 8,
-      span: 16,
-    },
-  };
+const { TabPane } = Tabs;
 
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+function LogInForm(props) {
   async function onFinish(values) {
-    console.log(values);
+    // console.log(values);
     try {
       const apiEndpoint = window.ENV.UI_AUTH_ENDPOINT;
       const response = await fetch(`${apiEndpoint}/login`, {
@@ -70,6 +72,62 @@ function LogInForm(props) {
       <Form.Item {...tailLayout}>
         <Button type="primary" htmlType="submit">
           Log in
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+function RegisterForm() {
+  function onFinish(values) {
+    console.log(values);
+  }
+
+  return (
+    <Form
+      {...layout}
+      name="register"
+      onFinish={onFinish}
+    >
+      <Form.Item
+        label="Username"
+        name="name"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your email!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Register
         </Button>
       </Form.Item>
     </Form>
@@ -144,7 +202,7 @@ export default class LogInNavItem extends React.Component {
         <>
           <Avatar size="large" icon={<UserOutlined />} />
           <Dropdown overlay={menu} trigger={['click']}>
-            <Button>
+            <Button ghost>
               {user.name}
               <DownOutlined />
             </Button>
@@ -158,12 +216,19 @@ export default class LogInNavItem extends React.Component {
           Login
         </Button>
         <Modal
-          title="Log In"
+          title="Login/Register"
           visible={visible}
           onCancel={this.hideModal}
           footer={null}
         >
-          <LogInForm login={this.login} />
+          <Tabs defaultActiveKey="1" centered>
+            <TabPane tab="Login" key="1">
+              <LogInForm login={this.login} />
+            </TabPane>
+            <TabPane tab="Register" key="2">
+              <RegisterForm />
+            </TabPane>
+          </Tabs>
         </Modal>
       </>
     );
