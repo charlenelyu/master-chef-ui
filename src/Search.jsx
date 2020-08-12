394c40f3fa685928472cdf31164140edc25de046
Tianhui Li <li.tianhu@husky.neu.edu>
2020-08-12 12:09:16 -0700

    reconstruct search bar using Select component from antd

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
-import SelectAsync from 'react-select/lib/Async'; // eslint-disable-line
+import { Select } from 'antd';
 import { withRouter } from 'react-router-dom';
 
 import graphQLFetch from './graphQLFetch.js';
 
+const { Option } = Select;
+
 class Search extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      recipes: [],
+      value: [],
+    };
     this.onChangeSelection = this.onChangeSelection.bind(this);
     this.loadOptions = this.loadOptions.bind(this);
   }
 
-  onChangeSelection({ value }) {
+  onChangeSelection(value) {
     const { history } = this.props;
     history.push(`/view/${value}`);
   }
 
+  // eslint-disable-next-line consistent-return
   async loadOptions(term) {
     if (term.length < 3) return [];
     const query = `query recipeList($search: String) {
@@ -27,21 +33,31 @@ class Search extends React.Component {
 
     const { showError } = this.props;
     const data = await graphQLFetch(query, { search: term }, showError);
-    return data.recipeList.map(recipe => ({
-      label: `#${recipe.id}: ${recipe.title}`, value: recipe.id,
+    const recipes = data.recipeList.map(recipe => ({
+      text: `#${recipe.id}: ${recipe.title}`, value: recipe.id,
     }));
+    this.setState({
+      recipes,
+    });
   }
 
   render() {
+    const { recipes, value } = this.state;
+
     return (
-      <SelectAsync
-        instanceId="search-select"
-        value=""
-        loadOptions={this.loadOptions}
+      <Select
+        mode="multiple"
+        value={value}
+        placeholder="Search by Title"
         filterOption={() => true}
+        onSearch={this.loadOptions}
         onChange={this.onChangeSelection}
-        components={{ DropdownIndicator: null }}
-      />
+        style={{ width: '100%', marginTop: 15 }}
+      >
+        {recipes.map(recipe => (
+          <Option key={recipe.value}>{recipe.text}</Option>
+        ))}
+      </Select>
     );
   }
 }
