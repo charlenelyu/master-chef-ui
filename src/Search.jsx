07fdda6e041a15ce083e5436632260eeca9f9c38
Tianhui Li <li.tianhu@husky.neu.edu>
2020-08-12 04:33:18 -0700

    search bar integrate with api

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,1 +1,49 @@
-// leave for Search page
+import React from 'react';
+import SelectAsync from 'react-select/lib/Async'; // eslint-disable-line
+import { withRouter } from 'react-router-dom';
+
+import graphQLFetch from './graphQLFetch.js';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeSelection = this.onChangeSelection.bind(this);
+    this.loadOptions = this.loadOptions.bind(this);
+  }
+
+  onChangeSelection({ value }) {
+    const { history } = this.props;
+    history.push(`/view/${value}`);
+  }
+
+  async loadOptions(term) {
+    if (term.length < 3) return [];
+    const query = `query recipeList($search: String) {
+      recipeList(search: $search) {
+        id title
+      }
+    }`;
+
+    const { showError } = this.props;
+    const data = await graphQLFetch(query, { search: term }, showError);
+    return data.recipeList.map(recipe => ({
+      label: `#${recipe.id}: ${recipe.title}`, value: recipe.id,
+    }));
+  }
+
+  render() {
+    return (
+      <SelectAsync
+        instanceId="search-select"
+        value=""
+        loadOptions={this.loadOptions}
+        filterOption={() => true}
+        onChange={this.onChangeSelection}
+        components={{ DropdownIndicator: null }}
+      />
+    );
+  }
+}
+
+export default withRouter(Search);
