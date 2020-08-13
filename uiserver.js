3980ad6a26faa7653f44253461a9c5027296994c
charlenelyu <lyu.yangh@northeastern.edu>
2020-08-12 23:32:51 -0700

    try to fix cors

--- uiserver.js
+++ uiserver.js
@@ -1,16 +1,34 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 
 app.use(express.static('public'));
 
-const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT
-  || 'http://localhost:3000/graphql';
-const UI_AUTH_ENDPOINT = process.env.UI_AUTH_ENDPOINT
-  || 'http://localhost:3000/auth';
-const env = { UI_API_ENDPOINT, UI_AUTH_ENDPOINT };
+const apiProxyTarget = process.env.API_PROXY_TARGET;
+if (apiProxyTarget) {
+  app.use('/graphql', proxy({ target: apiProxyTarget, changeOrigin: true }));
+  app.use('/auth', proxy({ target: apiProxyTarget, changeOrigin: true }));
+}
+
+if (!process.env.UI_API_ENDPOINT) {
+  process.env.UI_API_ENDPOINT = 'http://localhost:3000/graphql';
+}
+
+if (!process.env.UI_SERVER_API_ENDPOINT) {
+  process.env.UI_SERVER_API_ENDPOINT = process.env.UI_API_ENDPOINT;
+}
+
+if (!process.env.UI_AUTH_ENDPOINT) {
+  process.env.UI_AUTH_ENDPOINT = 'http://localhost:3000/auth';
+}
+
+const env = {
+  UI_API_ENDPOINT: process.env.UI_API_ENDPOINT,
+  UI_AUTH_ENDPOINT: process.env.UI_AUTH_ENDPOINT,
+};
 
 app.get('/env.js', (req, res) => {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
